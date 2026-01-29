ee7bf8b3fc5697a723b2a52551d81612c1cf30ab
TonglinYang777 <W0526437@campus.nscc.ca>
2026-01-29 07:29:54 -0800

    Add script.js

---
+++ Client Side/Assignment2-Client-Side/script.js
@@ -0,0 +1,19 @@
+let contact1Name = "Alice Johnson";
+let contact1Phone = "902-555-1234";
+let contact1Email = "alice@example.com";
+
+let contact2Name = "Mark Smith";
+let contact2Phone = "902-555-5678";
+let contact2Email = "mark@example.com";
+
+let contact3Name = "Sara Brown";
+let contact3Phone = "902-555-9012";
+let contact3Email = "sara@example.com";
+
+document.getElementById("firstContactName").textContent = contact1Name;
+document.getElementById("firstContactPhone").textContent = contact1Phone;
+document.getElementById("firstContactEmail").textContent = contact1Email;
+
+document.getElementById("lastContactName").textContent = contact3Name;
+document.getElementById("lastContactPhone").textContent = contact3Phone;
+document.getElementById("lastContactEmail").textContent = contact3Email;
